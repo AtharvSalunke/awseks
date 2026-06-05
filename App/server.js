453f424780c671c49d54d5b89d1a6c36baa38ae3
Atharv Salunke <atharvs@eshopmarketing.in>
2026-06-05 13:10:09 +0530

    implementing prometheus and granfana

--- App/server.js
+++ App/server.js
@@ -1,7 +1,38 @@
 const express = require("express");
+const client = require("prom-client");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Create Registry
+const register = new client.Registry();
+
+// Collect default metrics
+client.collectDefaultMetrics({
+    register
+});
+
+// Custom HTTP Request Counter
+const httpRequestsTotal = new client.Counter({
+    name: "http_requests_total",
+    help: "Total number of HTTP requests",
+    labelNames: ["method", "route", "status"]
+});
+
+register.registerMetric(httpRequestsTotal);
+
+// Middleware
+app.use((req, res, next) => {
+    res.on("finish", () => {
+        httpRequestsTotal.inc({
+            method: req.method,
+            route: req.route?.path || req.path,
+            status: res.statusCode
+        });
+    });
+
+    next();
+});
 
 // Health endpoint
 app.get("/health", (req, res) => {
@@ -14,7 +45,7 @@ app.get("/health", (req, res) => {
 // Main endpoint
 app.get("/", (req, res) => {
     res.status(200).json({
-        message: "Hello from EKS "
+        message: "Hello from EKS"
     });
 });
 
@@ -30,6 +61,12 @@ app.get("/api/users", (req, res) => {
             name: "John"
         }
     ]);
+});
+
+// Prometheus metrics endpoint
+app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", register.contentType);
+    res.end(await register.metrics());
 });
 
 app.listen(PORT, () => {
